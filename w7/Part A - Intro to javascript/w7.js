16442a9b738a490ee22ec236aad380fe251fa490
SentientSchnitzel <simondanielschneider@gmail.com>
2022-10-12 00:19:21 +1100

    initial files added back

---
+++ w7/Part A - Intro to javascript/w7.js
@@ -0,0 +1,14 @@
+var number = 10
+
+typeof number
+
+var t1 = false == 0 // returns true
+var t2 = false === 0 // returns false, as its not the same type but it is same value
+
+a = [0,1,2,3,4] // list, type: object
+
+a = 1 + 2 + 3 // = 6
+a = 1 + "2" + "3" // = "6"
+
+console.log(number, a, typeof a, t1, t2)
+
